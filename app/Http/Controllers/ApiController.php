439faf9efe0450cedd981a2879ece0dfc0942cfc
Nur Diantoro <nurdiantoro100@gmail.com>
2025-02-21 20:24:28 +0700

    1.37 Public API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,19 +2,81 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Career;
 use App\Models\Event;
-use Illuminate\Http\Request;
+use App\Models\EventCarousel;
+use App\Models\EventImage;
+use App\Models\Inbox;
+use App\Models\News;
+use App\Models\NewsCarousel;
+use App\Models\NextEvent;
+use App\Models\Partner;
+use App\Models\Team;
+use App\Models\Testimoni;
+use App\Models\Youtube;
 
 class ApiController extends Controller
 {
+    public function career()
+    {
+        $data = Career::all();
+        return response()->json($data);
+    }
     public function event()
     {
         $data = Event::all();
-        return $data;
+        return response()->json($data);
     }
-    public function event_detail($id)
+    public function event_carousel()
     {
-        $data = Event::find($id);
+        $data = EventCarousel::all();
+        return response()->json($data);
+    }
+    public function event_image()
+    {
+        $data = EventImage::all();
+        return response()->json($data);
+    }
+    public function inbox()
+    {
+        $data = Inbox::all();
+        return response()->json($data);
+    }
+    public function news()
+    {
+        $data = News::all();
+        return response()->json($data);
+    }
+    public function news_carousel()
+    {
+        $data = NewsCarousel::all();
+        return response()->json($data);
+    }
+    public function next_event()
+    {
+        $data = NextEvent::all();
         return $data;
+        return response()->json($data);
+    }
+
+    public function partner()
+    {
+        $data = Partner::select('id', 'logo')->get();
+        return response()->json($data);
+    }
+    public function team()
+    {
+        $data = Team::all();
+        return response()->json($data);
+    }
+    public function testimoni()
+    {
+        $data = Testimoni::all();
+        return response()->json($data);
+    }
+    public function youtube()
+    {
+        $data = Youtube::all();
+        return response()->json($data);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,17 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/career', [ApiController::class, 'career']);
 Route::get('/event', [ApiController::class, 'event']);
 Route::get('/event/{id}', [ApiController::class, 'event_detail']);
+
+Route::get('/event_carousel', [ApiController::class, 'event_carousel']);
+Route::get('/event_image', [ApiController::class, 'event_image']);
+Route::get('/inbox', [ApiController::class, 'inbox']);
+Route::get('/news', [ApiController::class, 'news']);
+Route::get('/news_carousel', [ApiController::class, 'news_carousel']);
+Route::get('/next_event', [ApiController::class, 'next_event']);
+Route::get('/partner', [ApiController::class, 'partner']);
+Route::get('/team', [ApiController::class, 'team']);
+Route::get('/testimoni', [ApiController::class, 'testimoni']);
+Route::get('/youtube', [ApiController::class, 'youtube']);
